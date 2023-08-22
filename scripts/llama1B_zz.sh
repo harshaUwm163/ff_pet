4bc4c00151a3f7817f113815b6749638300962eb
HARSHAVARDHAN ADEPU <harshavardhan@olvi-2.datascience.wisc.edu>
2023-08-22 16:38:24 -0500

    updated scripts with b_size=4

--- scripts/llama1B_zz.sh
+++ scripts/llama1B_zz.sh
@@ -1,7 +1,7 @@
 script_path="$(realpath "$0")"
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.run --nproc-per-node 8  torchrun_main.py \
     --model_config configs/llama_1b.json \
-    --batch_size 8 \
+    --batch_size 4 \
     --total_batch_size 320 \
     --lr 5e-4 \
     --max_length 1024 \
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.run --nproc-per
     --num_training_steps 40000 \
     --save_every 3000 \
     --eval_every 3000 \
-    --continue_from /data/harsha/relora_exps_olvi2/model1b/warmup_20k_gaccum5/model_5000 \
+    --continue_from <path/to/checkpoint> \
     --tags relora_1b \
     --exp_name tff_1b_40k_8gpus_lr5em4_gaccum5_int5460 \
     --script_path $script_path
